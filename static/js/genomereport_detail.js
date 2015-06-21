e19cc9506791daab01f1efc36cc6c351dbf9d9be
Madeleine Ball <mpball@gmail.com>
2015-06-20 23:30:23 -0400

    Use more GenNotes to update genome report

--- static/js/genomereport_detail.js
+++ static/js/genomereport_detail.js
@@ -1,24 +1,98 @@
+var add_id_data = function (b37ID, clinvarRelationsData, template, elem) {
+  var varNameString = ''
+  var varNames = []
+  clinvarRelationsData.forEach(function (clinvarData) {
+    var prefName = clinvarData['clinvar-rcva:preferred-name']
+    if (!prefName) return
+
+    if (varNameString) {
+      // If this name already listed, don't output again.
+      if (_.contains(varNames, prefName)) return
+      varNameString = varNameString + '<br><small>or</small><br><b>' +
+                      prefName + '</b>'
+    } else {
+      varNameString = '<b>' + clinvarData['clinvar-rcva:preferred-name']
+    }
+    varNames.push(prefName)
+  })
+  // console.log(varNames)
+  var templated = template({
+    clinvarRCVAPreferredName: varNameString,
+    b37VariantID: b37ID
+  })
+  elem.append(templated)
+  // console.log(elem)
+}
+
+var add_freq_data = function (clinvarRelationsData, template, elem) {
+  var frequencies = []
+  clinvarRelationsData.forEach(function (clinvarData) {
+    if ('clinvar-rcva:esp-allele-frequency' in clinvarData) {
+      var freq = clinvarData['clinvar-rcva:esp-allele-frequency']
+      if (_.contains(frequencies, freq)) return
+      frequencies.push(freq)
+    }
+  })
+  // console.log(frequencies)
+  var templated
+  if (frequencies.length === 0) {
+    templated = template({clinvarRCVAfreqESP: 'Unknown'})
+  } else {
+    templated = template({clinvarRCVAfreqESP: frequencies[0]})
+  }
+  elem.append(templated)
+  // console.log(elem)
+}
+
+var add_info_data = function (clinvarRelationsData, template, elem) {
+  clinvarRelationsData.forEach(
+    function (clinvarData) {
+      var templated = template({
+        clinvarRCVAAccession: clinvarData['clinvar-rcva:accession'],
+        clinvarRCVADiseaseName: clinvarData['clinvar-rcva:disease-name'],
+        clinvarRCVASignificance: clinvarData['clinvar-rcva:significance']
+      })
+      // console.log(templated)
+      elem.append(templated)
+    }
+  )
+  // console.log(elem)
+}
+
 var add_gennotes_data = function (data, textStatus, jqXHR) {
   for (var i = 0; i < data['results'].length; i++) {
     var result = data['results'][i]
+    // console.log(result['b37_id'])
+
+    // Set up target elements by emptying them.
     var rowGV = $('tr#gv-' + result['b37_id'])
-    var elemGV = rowGV.find('td.gv-info-cell').empty()
-    var variantDataTemplateHTML = $('#variant-data-template').html()
-    var templateVariantData = _.template(variantDataTemplateHTML)
-    for (var j = 0; j < result['relation_set'].length; j++) {
-      var templated = templateVariantData({
-        clinvarRCVAAccession: result['relation_set'][j]['tags']['clinvar-rcva:accession'],
-        clinvarRCVADiseaseName: result['relation_set'][j]['tags']['clinvar-rcva:disease-name'],
-        clinvarRCVASignificance: result['relation_set'][j]['tags']['clinvar-rcva:significance']
-      })
-      elemGV.append(templated)
-      console.log(variantDataTemplateHTML)
-      console.log(templated)
-    }
+    var elemGVID = rowGV.find('td.gv-id-cell').empty()
+    var elemGVFreq = rowGV.find('td.gv-freq-cell').empty()
+    var elemGVInfo = rowGV.find('td.gv-info-cell').empty()
+
+    // Load appropriate templates.
+    var templateVariantID = _.template($('#variant-id-template').html())
+    var templateVariantFreq = _.template($('#variant-freq-template').html())
+    var templateVariantInfo = _.template($('#variant-info-template').html())
+
+    // Extract list of Relations of type 'clinvar-rcva'.
+    var clinvarRelationsData = []
+    result['relation_set'].forEach(function (relation) {
+      if (relation['tags']['type'] === 'clinvar-rcva') {
+        clinvarRelationsData.push(relation['tags'])
+      }
+    })
+
     // No RCVA matches, prob variant was a multi-allele record - ignore it.
-    if (result['relation_set'].length === 0) {
+    if (clinvarRelationsData.length === 0) {
       rowGV.addClass('hidden')
+      continue
     }
+
+    // console.log('Adding ClinVar data')
+    add_id_data(result['b37_id'], clinvarRelationsData, templateVariantID, elemGVID)
+    add_freq_data(clinvarRelationsData, templateVariantFreq, elemGVFreq)
+    add_info_data(clinvarRelationsData, templateVariantInfo, elemGVInfo)
   }
 }
 
@@ -26,13 +100,13 @@ $(function () {
   var variantList = $('tr.gv-row').map(function () {
     return $(this).attr('id').substring(3)
   }).toArray()
-  console.log(variantList)
+  // console.log(variantList)
   var variantListJSON = JSON.stringify(
     variantList.map(function (v) {
         return 'b37-' + v
       })
     )
-  console.log(variantListJSON)
+  // console.log(variantListJSON)
   $.ajax(
     {
       url: 'http://gennotes.herokuapp.com/api/variant/',
